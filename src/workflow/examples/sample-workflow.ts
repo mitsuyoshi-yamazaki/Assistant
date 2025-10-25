ec99cc9902c44cc75fef6b063998fa4da62e5514
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2025-10-25 18:08:33 +0900

    直接activityを呼び出していたworkflowを修正

--- src/workflow/examples/sample-workflow.ts
+++ src/workflow/examples/sample-workflow.ts
@@ -1,12 +1,16 @@
-import { Workflow, upsertSearchAttributes } from "@temporalio/workflow"
-import { sampleActivity } from "../../activity/sample-activity"
+import { Workflow, upsertSearchAttributes, proxyActivities } from "@temporalio/workflow"
+import type * as activities from "../../activity/activities"
+
+const { sampleActivity } = proxyActivities<typeof activities>({
+  startToCloseTimeout: "1 minute",
+})
 
 export const sampleWorkflow: Workflow = async (argument: number): Promise<string> => {
   upsertSearchAttributes({
     SampleWorkflowArgument: [argument],
   })
 
-  return new Promise(resolve => {
-    resolve(sampleActivity(argument))
-  })
+  // const sampleActivity: symbol と解釈されるため、型アサーションを使用
+  const result = await (sampleActivity as unknown as (value: number) => Promise<string>)(argument)
+  return result
 }
